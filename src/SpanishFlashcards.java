ef333cdb4c2fb1844858bf566195aed7992d42d5
Kevin Korte <kkorte11@gmail.com>
2016-06-15 12:55:02 -0400

    Moved classes to their own files

--- src/SpanishFlashcards.java
+++ src/SpanishFlashcards.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.util.ArrayList;
@@ -8,15 +7,16 @@ import java.util.Scanner;
 
 public class SpanishFlashcards {
 
+	//TODO move score mods to one location, currently in main and Card
 	static final int incorrectMod = -3, correctMod = 1;
-	Scanner in, userIn;
+	Scanner userIn;
 	FileOutputStream out;
 	final static String source = (new File("")).getAbsolutePath() + "\\ch11.txt";
 	FlashcardStack cardStack;
 
 	public SpanishFlashcards() {
 		cardStack = new FlashcardStack();
-		cardStack.createStack();
+		cardStack.createStack(source);
 
 		userIn = new Scanner(System.in);
 		String userChoice = "";
@@ -80,11 +80,10 @@ public class SpanishFlashcards {
 	}
 
 	private void spanToEng() {
-		//TODO
 		Random rand = new Random();
-		FlashcardStack.OrderedStackList cur = cardStack.getSpanishLowest();
-		ArrayList<FlashcardStack.Card> curCards = cur.getCards();
-		FlashcardStack.Card curC; 
+		OrderedStackList cur = cardStack.getSpanishLowest();
+		ArrayList<Card> curCards = cur.getCards();
+		Card curC; 
 		int curSize = curCards.size();
 		int randChoice;
 		String userChoice = "";
@@ -172,346 +171,6 @@ public class SpanishFlashcards {
 			out.close();
 		}catch(Exception e){
 			System.out.println("Error accessing path: " + e);
-		}
-	}
-
-
-	public class FlashcardStack{
-
-		OrderedStackList spanStack, engStack;
-
-		private void createStack() {
-			try {
-				in = new Scanner(new File(source));
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-
-			String[] inputStr;
-			String eng, span;
-			int engScore, spanScore;
-
-			while(in.hasNext()){
-				inputStr = in.nextLine().split("/");
-				span = inputStr[0];
-				eng = inputStr[1];
-				spanScore = Integer.parseInt(inputStr[2]);
-				engScore = Integer.parseInt(inputStr[3]);
-				if(spanStack == null){
-					spanStack = new OrderedStackList(null, null, spanScore, new Card(span,eng,spanScore,engScore));
-				} else {
-					spanStack.addCardSpan(new Card(span,eng,spanScore,engScore));
-				}
-				if(engStack == null){
-					engStack = new OrderedStackList(null, null, spanScore, new Card(span,eng,spanScore,engScore));
-				} else {
-					engStack.addCardEng(new Card(span,eng,spanScore,engScore));
-				}	
-				getSpanishLowest().printScoreDist();
-				System.out.println();
-			}
-
-			in.close();
-		}
-
-		public void printSpnDist() {
-			spanStack.printScoreDist();
-		}
-
-		public void removeSpanishRow(int s) {
-			spanStack = spanStack.getLowest().removeRow(s);
-		}
-
-		public OrderedStackList getSpanishHighest() {
-			return spanStack.getHighest();
-		}
-
-		public OrderedStackList getSpanishLowest() {
-			return spanStack.getLowest();
-		}
-
-		public void addCard(String spanish, String english){
-			spanStack.addCardEng(new Card(spanish, english, 0, 0));
-			engStack.addCardEng(new Card(spanish, english, 0, 0));
-		}
-
-		public ArrayList<String> getCards() {
-			return spanStack.printStack();
-		}
-
-		public class OrderedStackList {
-			OrderedStackList greater, lesser;
-			ArrayList<Card> cards;
-			int score;
-
-			public OrderedStackList(OrderedStackList g, OrderedStackList l, int s, Card c) {
-				greater = g;
-				lesser = l;
-				score = s;
-				cards = new ArrayList<Card>();
-				cards.add(c);
-			}
-
-			public OrderedStackList removeRow(int s) {
-				if(score == s){
-					if(greater == null){
-						if(lesser != null){
-							lesser.greater = null;
-							return lesser;
-						} else {
-							return null;
-						}
-					}
-
-					if(lesser == null){
-						if(greater != null){
-							greater.lesser = null;
-							return greater;
-						} else {
-							return null;
-						}
-					}
-
-					lesser.greater = greater;
-					greater.lesser = lesser;
-					return lesser;
-
-
-				} else if (s > score){
-					if(greater != null){
-						return greater.removeRow(s);
-					} else {
-						return null;
-					}
-				} else {
-					if(lesser != null){
-						return lesser.removeRow(s);
-					} else {
-						return null;
-					}
-				}
-
-			}
-
-			public void printScoreDist() {
-				if(lesser == null){
-					System.out.println(score + " " + cards.size());
-				} else {
-					System.out.println(score + " " + cards.size());
-					lesser.printScoreDist();
-				}
-			}
-
-			public void moveCard(Card c, int target) {
-				if(target == score){
-					cards.add(c);
-				} else if(target > score){
-					if(greater != null){
-						if(greater.getScore() > target && target > score){
-							OrderedStackList temp = new OrderedStackList(greater, this, target, c);
-							greater.setLesser(temp);
-							greater = temp;
-						} else {
-							greater.moveCard(c, target);
-						}
-					} else {
-						greater = new OrderedStackList(null, this, target, c);
-					}
-				} else {
-					if(lesser != null){
-						if(lesser.getScore() < target && target < score){
-							OrderedStackList temp = new OrderedStackList(this, lesser, target, c);
-							lesser.setGreater(temp);
-							lesser = temp;
-						} else {
-							lesser.moveCard(c, target);
-						}
-					} else {
-						lesser = new OrderedStackList(this, null, target, c);
-					}
-				}
-			}
-
-			private void setLesser(OrderedStackList l) {
-				lesser = l;
-			}
-
-			private void setGreater(OrderedStackList g) {
-				greater = g;
-			}
-
-			public ArrayList<Card> getCards(){
-				return cards;
-			}
-
-			public int getScore(){
-				return score;
-			}
-
-			public OrderedStackList getHighest(){
-				if(greater == null){
-					return this;
-				} else {
-					return greater.getHighest();
-				}
-			}
-
-			public OrderedStackList getLowest(){
-				if(lesser == null){
-					return this;
-				} else {
-					return lesser.getLowest();
-				}
-			}
-
-
-			private int getSize() {
-				return cards.size();
-			}
-
-			private boolean hasGreater() {
-				if(greater == null){
-					return false;
-				}
-				return true;
-			}
-
-			private boolean hasLesser() {
-				if(lesser == null){
-					return false;
-				}
-				return true;
-			}
-
-			public ArrayList<String> printStack(){
-				if(greater == null){
-					return printStack(new ArrayList<String>());
-				} else {
-					return greater.printStack();
-				}
-			}
-
-			public ArrayList<String> printStack(ArrayList<String> p){
-				if(lesser == null){
-					return p;
-				} else {
-					for(Card c: cards){
-						p.add(c.toString() + "\n");
-					}
-					return lesser.printStack(p);
-				}
-			}
-
-			public void addCardSpan(Card c){
-				int newScore = c.getSpanScore();
-				if(newScore == score){
-					cards.add(c);
-				} else if(newScore > score){
-					if(greater == null){
-						greater = new OrderedStackList(null, this, newScore, c);
-					} else {
-						if(greater.score > newScore){
-							OrderedStackList newG = new OrderedStackList(this.greater, this, newScore, c);
-							greater.lesser = newG;
-							greater = newG;
-						} else {
-							greater.addCardSpan(c);
-						}
-					}
-				} else {
-					if(lesser == null){
-						lesser = new OrderedStackList(this, null, newScore, c);
-					} else {
-						if(lesser.score < newScore){
-							OrderedStackList newG = new OrderedStackList(this, this.lesser, newScore, c);
-							lesser.greater = newG;
-							lesser = newG;
-						} else {
-							lesser.addCardSpan(c);
-						}
-					}
-				}
-			}
-
-			public void addCardEng(Card c){
-				int newScore = c.getEngScore();
-				if(newScore == score){
-					cards.add(c);
-				} else if(newScore > score){
-					if(greater == null){
-						greater = new OrderedStackList(null, this, newScore, c);
-					} else {
-						if(greater.score != newScore){
-							OrderedStackList newG = new OrderedStackList(this.greater, this, newScore, c);
-							greater.lesser = newG;
-							greater = newG;
-						} else {
-							greater.addCardEng(c);
-						}
-					}
-				} else {
-					if(lesser == null){
-						lesser = new OrderedStackList(this, null, newScore, c);
-					} else {
-						if(lesser.score != newScore){
-							OrderedStackList newG = new OrderedStackList(this, this.lesser, newScore, c);
-							lesser.greater = newG;
-							lesser = newG;
-						} else {
-							lesser.addCardEng(c);
-						}
-					}
-				}
-			}
-		}
-
-		private class Card {
-
-			String spanish, english;
-			int spnScore, engScore;
-
-			public Card(String spn, String eng, int spnS, int engS){
-				spanish = spn;
-				english = eng;
-				spnScore = spnS;
-				engScore = engS;
-			}
-
-			public int getEngScore() {
-				return engScore;
-			}
-
-			public int getSpanScore() {
-				return spnScore;
-			}
-
-			public String toString(){
-				return spanish + "/" + english + "/" + spnScore + "/" + engScore;
-			}
-
-			public String getSpanish(){
-				return spanish;
-			}
-
-			public String getEnglish(){
-				return english;
-			}
-
-			public void spnIncorrect(){
-				spnScore += incorrectMod;
-			}
-
-			public void spnCorrect(){
-				spnScore += correctMod;
-			}
-
-			public void engIncorrect(){
-				engScore += incorrectMod;
-			}
-
-			public void engCorrect(){
-				engScore += correctMod;
-			}
 		}
 	}
 }
